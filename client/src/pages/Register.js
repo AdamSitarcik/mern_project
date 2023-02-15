20a9761807fd68fc02778c71e19650b223ed7916
Adam Sitarcik <adamsitarcik21@gmail.com>
2023-02-15 15:50:19 +0100

    add toggling between register and login

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -6,13 +6,17 @@ const initialState = {
     name: '',
     email: '',
     password: '',
-    isMember: true,
+    isMember: false,
     showAlert: false
 }
 
 function Register() {
     const [values, setValues] = useState(initialState)
     // global state and useNavigate
+
+    const toggleMember = () => {
+        setValues({ ...values, isMember: !values.isMember })
+    }
 
     const handleChange = (e) => {
         console.log(e.target.value);
@@ -27,12 +31,16 @@ function Register() {
         <Wrapper className='full-page'>
             <form className='form' action='' onSubmit={onSubmit}>
                 <Logo />
-                <h3>Login</h3>
+                <h3>{values.isMember ? 'Login' : 'Register'}</h3>
                 {values.showAlert && <Alert />}
-                <FormRow name='name' type='text' value={values.name} labelText='Name' handleChange={handleChange} />
+                {!values.isMember && <FormRow name='name' type='text' value={values.name} labelText='Name' handleChange={handleChange} />}
                 <FormRow name='email' type='email' value={values.email} labelText='Email' handleChange={handleChange} />
                 <FormRow name='password' type='password' value={values.password} labelText='Password' handleChange={handleChange} />
                 <button type='submit' className='btn btn-block'>Submit</button>
+                <p>
+                    {values.isMember ? 'Not a member yet?' : 'Already a member?'}
+                    <button type='button' onClick={toggleMember} className='member-btn'>{values.isMember ? 'Register' : 'Login'}</button>
+                </p>
             </form>
         </Wrapper>
     )
