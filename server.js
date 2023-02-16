40487f9191439f1c8d03edc8079d0d23686697d6
Adam Sitarcik <adamsitarcik21@gmail.com>
2023-02-16 22:58:01 +0100

    setup auth structure

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 import express from 'express';
+
+import dotenv from 'dotenv';
+
+// db and authenticateUser
+import connectDB from './db/connect.js';
+
+// routers
+import authRouter from './db/connect.js';
+
+// middleware
 import errorHandlerMiddleware from './middleware/error-handler.js';
 import notFoundMiddleware from './middleware/not-found.js';
-import dotenv from 'dotenv';
-import connectDB from './db/connect.js';
+
+app.use(express.json());
 
 dotenv.config();
 
@@ -12,18 +22,20 @@ app.get('/', (req, res) => {
     res.send('Welcome');
 });
 
+app.use('/api/v1/auth', authRouter);
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
 const start = async () => {
-    try{
+    try {
         await connectDB(process.env.MONGO_URL);
         app.listen(port, () => console.log(`Server listening on port ${port}...`));
     } catch (error) {
         console.log(error);
     }
-}
+};
 
 start();
