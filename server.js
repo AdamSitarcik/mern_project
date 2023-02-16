866307ebe96d251876448d7ae5c2edf847c9effc
Adam Sitarcik <adamsitarcik21@gmail.com>
2023-02-16 22:45:52 +0100

    setup mongodb

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 import notFoundMiddleware from './middleware/not-found.js';
+import dotenv from 'dotenv';
+import connectDB from './db/connect.js';
+
+dotenv.config();
 
 const app = express();
 
 app.get('/', (req, res) => {
-    throw new Error('error');
     res.send('Welcome');
 });
 
@@ -14,6 +17,13 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server listening on port ${port}...`));
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGO_URL);
+        app.listen(port, () => console.log(`Server listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-
+start();
