d5f0b79eb2ffd58a1e1cfd210dc72129a4929989
Adam Sitarcik <adamsitarcik21@gmail.com>
2023-02-18 22:45:39 +0100

    setup BadRequest and NotFound error classes

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,16 +2,29 @@ import User from "../models/User.js";
 import { StatusCodes } from 'http-status-codes';
 
 class CustomAPIError extends Error {
-    constructor(message){
+    constructor(message) {
         super(message);
-        this.statusCode = StatusCodes.BAD_REQUEST;
+    }
+}
+
+class BadRequestError extends CustomAPIError {
+    constructor(message) {
+        super(message);
+        this.statusCode = StatusCodes.BAD_REQUEST
+    }
+}
+
+class NotFoundError extends CustomAPIError {
+    constructor(message) {
+        super(message);
+        this.statusCode = StatusCodes.NOT_FOUND;
     }
 }
 
 const register = async (req, res) => {
     const { name, email, password } = req.body;
     if (!name || !email || !password) {
-        throw new CustomAPIError('Something is missing');
+        throw new BadRequestError('Something is missing');
     }
     const user = await User.create({ name, email, password });
     res.status(StatusCodes.OK).json({ user });
