ff74a97c0985af471c90156d7e762999175ce93f
Adam Sitarcik <adamsitarcik21@gmail.com>
2023-02-18 21:59:55 +0100

    check for missing values in controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,14 @@
 import User from "../models/User.js";
-import {StatusCodes} from 'http-status-codes';
+import { StatusCodes } from 'http-status-codes';
 
 
 const register = async (req, res) => {
-    const user = await User.create(req.body);
-    res.status(StatusCodes.OK).json({user});
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+        throw new Error('Something is missing');
+    }
+    const user = await User.create({name, email, password});
+    res.status(StatusCodes.OK).json({ user });
 };
 
 const login = async (req, res) => {
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,9 @@
 import { StatusCodes } from 'http-status-codes';
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-    console.log(err);
     const defaultError = {
         statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-        msg: 'Something went wrong'
+        msg: err.message || 'Something went wrong'
     }
     if (err.name === 'ValidationError') {
         defaultError.statusCode = StatusCodes.BAD_REQUEST;
@@ -14,7 +13,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     }
     if (err.code && err.code === 11000) {
         defaultError.statusCode = StatusCodes.BAD_REQUEST;
-        defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique!`;
+        defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`;
     }
     res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 }
