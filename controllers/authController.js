1cd07d15dc3f449bf4f591a9f7e6686220a6ae13
Adam Sitarcik <adamsitarcik21@gmail.com>
2023-02-18 23:06:16 +0100

    check for duplicate email

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,9 +4,16 @@ import { NotFoundError, BadRequestError } from "../errors/index.js";
 
 const register = async (req, res) => {
     const { name, email, password } = req.body;
+
     if (!name || !email || !password) {
         throw new BadRequestError('Something is missing');
     }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        throw new BadRequestError('Email already in use');
+    }
+
     const user = await User.create({ name, email, password });
     res.status(StatusCodes.OK).json({ user });
 };
