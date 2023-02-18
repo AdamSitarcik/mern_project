1fa474914f86ac96f5f1f2f0f0290705bb1f6f7d
Adam Sitarcik <adamsitarcik21@gmail.com>
2023-02-18 22:04:41 +0100

    add customAPIError extension of Error class

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,19 @@
 import User from "../models/User.js";
 import { StatusCodes } from 'http-status-codes';
 
+class CustomAPIError extends Error {
+    constructor(message){
+        super(message);
+        this.statusCode = StatusCodes.BAD_REQUEST;
+    }
+}
 
 const register = async (req, res) => {
     const { name, email, password } = req.body;
     if (!name || !email || !password) {
-        throw new Error('Something is missing');
+        throw new CustomAPIError('Something is missing');
     }
-    const user = await User.create({name, email, password});
+    const user = await User.create({ name, email, password });
     res.status(StatusCodes.OK).json({ user });
 };
 
