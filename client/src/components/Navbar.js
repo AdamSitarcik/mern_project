f2d93df58b2c2cb0364b90e1ec590acee1e4b151
Adam Sitarcik <adamsitarcik21@gmail.com>
2023-02-26 17:19:36 -0600

    setup navbar buttons

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,34 @@
 import Wrapper from '../assets/wrappers/Navbar';
-import { FaHome } from 'react-icons/fa';
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
+import { useAppContext } from '../context/appContext';
+import { Logo } from '.';
 
 function Navbar() {
     return (
         <Wrapper>
-            <FaHome />
-            <h4>Navbar</h4>
+            <div className="nav-center">
+                <button type="button" className="toggle-btn" onClick={() => { console.log('toggle sidebar'); }}>
+                    <FaAlignLeft />
+                </button>
+                <div>
+                    <Logo />
+                    <h3 className="logo-text">
+                        dashboard
+                    </h3>
+                </div>
+                <div className="btn-container">
+                    <button type="button" className='btn' onClick={() => { console.log('show/hide dropdown'); }}>
+                        <FaUserCircle />
+                        John
+                        <FaCaretDown />
+                    </button>
+                    <div className="dropdown show-dropdown">
+                        <button type='button' className="dropdown-btn" onClick={() => { console.log('logout'); }}>
+                            logout
+                        </button>
+                    </div>
+                </div>
+            </div>
         </Wrapper>
     );
 }
