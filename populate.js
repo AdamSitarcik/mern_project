dd46f4b8db1a1b5f39e1c1ddcf41afd6bcc854de
Adam Sitarcik <adamsitarcik21@gmail.com>
2023-03-08 16:51:51 +0100

    populate db with mockeroo

---
+++ populate.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import { readFile } from 'fs/promises';
+
+dotenv.config();
+
+import connectDB from './db/connect.js';
+import Job from './models/Job.js';
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL);
+        await Job.deleteMany();
+        const jsonProducts = JSON.parse(await readFile(new URL('./moch-data.json', import.meta.url)));
+        await Job.create(jsonProducts);
+        console.log('Success');
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+start();
