87e3dabd0d5a5db9e01a278471559dcf942e96e0
Adam Sitarcik <adamsitarcik21@gmail.com>
2023-03-13 22:19:24 +0100

    setup debounce function in search container

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,26 +1,41 @@
 import { FormRow, FormRowSelect } from '.';
 import Wrapper from '../assets/wrappers/SearchContainer';
 import { useAppContext } from '../context/appContext';
+import { useState, useMemo } from 'react';
 
 function SearchContainer() {
+  const [localSearch, setLocalSearch] = useState('');
   const { isLoading, search, searchStatus, searchType, sort, sortOptions, handleChange, clearFilters, jobTypeOptions, statusOptions } = useAppContext();
 
   const handleSearch = (e) => {
-    if (isLoading) return;
     handleChange({ name: e.target.name, value: e.target.value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setLocalSearch('');
     clearFilters();
   };
+
+  const debounce = () => {
+    let timeoutID;
+    return (e) => {
+      setLocalSearch(e.target.value);
+      clearTimeout(timeoutID);
+      timeoutID = setTimeout(() => {
+        handleChange({ name: e.target.name, value: e.target.value });
+      }, 1000);
+    };
+  };
+
+  const optimisedDebounce = useMemo(() => debounce(), []);
 
   return (
     <Wrapper>
       <form className='form'>
         <h4>search form</h4>
         <div className="form-center">
-          <FormRow labelText='search position' type='text' name='search' value={search} handleChange={handleSearch} />
+          <FormRow labelText='search position' type='text' name='search' value={localSearch} handleChange={optimisedDebounce} />
           <FormRowSelect labelText='status' name='searchStatus' value={searchStatus} handleChange={handleSearch} list={['all', ...statusOptions]} />
           <FormRowSelect labelText='job type' name='searchType' value={searchType} handleChange={handleSearch} list={['all', ...jobTypeOptions]} />
           <FormRowSelect name='sort' value={sort} handleChange={handleSearch} list={sortOptions} />
